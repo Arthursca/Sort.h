b6a25a107dff385a0adbf53711f3a5eb125ef98f
Arthur Scanoni <arthur.scanoni@hotmail.com>
2021-03-19 11:54:47 -0300

    refactor: remove unnecessary steps

--- insertion_sort.c
+++ insertion_sort.c
@@ -5,7 +5,7 @@
 #include <math.h>
 
 long swap(void * a, void * b, size_t len);
-long insertion_sort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*));
+long isort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*));
 
 long swap(void * a, void * b, size_t len)
 {
@@ -18,17 +18,18 @@ long swap(void * a, void * b, size_t len)
     }
 }
 
-long insertion_sort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*))
+long isort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*))
 {
     long count = 0;
-    for (int i = 1, j; i < nitems; i++) {
+
+    for (int i = 1; i < nitems; i++) 
+    {
         for (int j = i-1; 0 <= j; j--)
 		{
             count++;
-            if(compar((base + size*i),(base + size*j)) < 0)
+            if(compar((base + size*(j + 1)),(base + size*j)) < 0)
             {
                 swap((base + size*(j + 1) ),(base + size*(j)), size);
-                i--;
             }
             else
             {
