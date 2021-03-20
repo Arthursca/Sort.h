3c9837d29975bd8d59488705581bffbbf8d5e7e1
Arthur Scanoni <arthur.scanoni@hotmail.com>
2021-03-20 13:57:20 -0300

    feat: create heap_sort

---
+++ heap_sort.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+long swap(void * a, void * b, size_t size);
+long hsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*));
+long heap(void *base, size_t size, int (*compar)(const void *, const void*),int n ,int i) ;
+
+long swap(void * a, void * b, size_t size)
+{
+    unsigned char * p = a, * q = b, tmp;
+    for (size_t i = 0; i != size; ++i)
+    {
+        tmp = p[i];
+        p[i] = q[i];
+        q[i] = tmp;
+    }
+}
+
+long hsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*))
+{
+    long count = 0;
+
+
+    for (int i = nitems / 2 - 1; i >= 0; i--)
+    {
+      count++;
+      count += heap(base, size, compar,nitems,i);
+    }
+
+    for (int i = nitems - 1; i >= 0; i--)
+    {
+      count++;
+      swap(base, base + i*size, size);
+      count += heap(base, size, compar, i, 0);
+    }
+
+    
+    return count;
+}
+
+long heap(void *base, size_t size, int (*compar)(const void *, const void*),int n ,int i) 
+{
+    long count = 0;
+    
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+  
+    if (left < n && compar(base + left*size , base + largest*size)  > 0)
+    {
+      largest = left;
+    }
+  
+    if (right < n && compar(base + right*size , base + largest*size) > 0)
+    {
+        largest = right;
+    }
+      
+  
+    if (largest != i) 
+    {
+      count++;
+      swap(base + i*size, base + largest*size, size);
+      count += heap(base,size,compar, n, largest);
+    }
+    return count;
+}
