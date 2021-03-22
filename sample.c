522f18f9ec8c3ca7a031f352a1d35136fa5af4d0
Arthur Scanoni <arthur.scanoni@hotmail.com>
2021-03-22 01:35:13 -0300

    feat: add comments

--- sample.c
+++ sample.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "sort.c"
 
+
+//Create the comparator
 int comparador(const void* a, const void* b)
 {
     return *(int*) a - *(int*) b;
@@ -9,11 +11,15 @@ int comparador(const void* a, const void* b)
 
 int main(void)
 {
+    //Create the array
     int arr[] = {2,6,14,25,13,25,46,85,42,92,99,45,32,01,25,65,23,41};
+    
+    //get the num of objects
     int arr_size = sizeof(arr)/sizeof(int);
-    long a = bsort(arr,arr_size ,sizeof(int), comparador);
 
-    printf("%d\n", a);
+    //sort the array and take the number of steps that was necessary for that 
+    long steps = bsort(arr,arr_size ,sizeof(int), comparador);
+    printf("%d\n", steps);
 
     for(int i = 0; i < arr_size; i++)
     {
