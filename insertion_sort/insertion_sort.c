a76b6448f5d2ea936c33e656584d59f41e4b6657
Arthur Scanoni <arthur.scanoni@hotmail.com>
2021-03-22 01:27:38 -0300

    feat: creates the insertion_sort folder

--- insertion_sort/insertion_sort.c
+++ insertion_sort/insertion_sort.c
@@ -1,22 +1,10 @@
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
+#include "..\utils\utils.c"
 
-long swap(void * a, void * b, size_t len);
+
 long isort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*));
 
-long swap(void * a, void * b, size_t len)
-{
-    unsigned char * p = a, * q = b, tmp;
-    for (size_t i = 0; i != len; ++i)
-    {
-        tmp = p[i];
-        p[i] = q[i];
-        q[i] = tmp;
-    }
-}
+
 
 long isort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*))
 {
