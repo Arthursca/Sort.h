524e0d8655cbf6a6ed1103c507fef1c4c50c6262
Arthur Scanoni <arthur.scanoni@hotmail.com>
2021-03-22 01:26:10 -0300

    feat: creates the bubble_sort folder

--- bubble_sort/bubble_sort.c
+++ bubble_sort/bubble_sort.c
@@ -1,22 +1,8 @@
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
+#include "..\utils\utils.c"
 
-long swap(void * a, void * b, size_t len);
+
 long bsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*));
-
-long swap(void * a, void * b, size_t len)
-{
-    unsigned char * p = a, * q = b, tmp;
-    for (size_t i = 0; i != len; ++i)
-    {
-        tmp = p[i];
-        p[i] = q[i];
-        q[i] = tmp;
-    }
-}
 
 long bsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*))
 {
@@ -26,7 +12,7 @@ long bsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, c
     for (int i = 1; i < nitems; i++) 
     {
         swapped = 0;
-        for (int j = 0; j < nitems-i-1; j++)
+        for (int j = 0; j <= nitems - (i + 1); j++)
 		{
             count++;
             if(compar((base + size*(j + 1)),(base + size*j)) < 0)
