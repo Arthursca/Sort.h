bbcbe8a482a6fc3fe521d5a40b742676c8ba2c32
Arthur Scanoni <arthur.scanoni@hotmail.com>
2021-03-23 17:14:20 -0300

    feat: add utils.h

--- utils/utils.c
+++ utils/utils.c
@@ -1,10 +1,10 @@
-#ifndef UTILS_
-#define UTILS_
-
 #include <stdio.h>
+#include "utils.h"
 
+void swap(void * a, void * b, size_t len);
+void copy(void * a, void * b, size_t len);
 
-long swap(void * a, void * b, size_t len)
+void swap(void * a, void * b, size_t len)
 {
     unsigned char * p = a, * q = b, tmp;
     for (size_t i = 0; i != len; ++i)
@@ -23,5 +23,3 @@ void copy(void * a, void * b, size_t len)
         p[i] = q[i];
     }
 }
-
-#endif
---
+++ utils/utils.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#ifndef UTILS_
+#define UTILS_
+void swap(void * a, void * b, size_t len);
+void copy(void * a, void * b, size_t len);
+#endif
